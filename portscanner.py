986ad844066c2b4d6f7234a91aea868a5ccd5fbd
Vincent2212 <80854326+Vincent2212@users.noreply.github.com>
2021-06-19 22:21:07 -0700

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -3,7 +3,7 @@ url = "https://ports.yougetsignal.com/check-port.php"
 how_many = int(input("How many ports to check? ")) # Enter a range starting from 1
 ip = input("Enter a IP Address: ")
 
-for x in range(1, how_many):
+for x in range(1, how_many + 1):
     data = {
         "MIME Type": "application/x-www-form-urlencoded; charset=UTF-8",
         "remoteAddress": ip,
@@ -12,7 +12,7 @@ for x in range(1, how_many):
     response = requests.post(url, data=data).text
     if 'src="/img/flag_green.gif' in response:
         print(f"Port {x} is open on {ip}")
-        break
+        
     elif "Invalid remote address." in response:
         print("Invalid IP or url.")
         break
